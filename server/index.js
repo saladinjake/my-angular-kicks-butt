bd24d12e8f328d16084046902ddbb1ff8dd264b9
saladinjake <juwavictor@gmail.com>
2020-09-05 21:50:11 +0100

    ch-#4 Created  sample controller  file for api server

--- server/index.js
+++ server/index.js
@@ -3,9 +3,13 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const { mongoose } = require('./db.js');
+var sampleController = require('./controllers/sampleController.js');
 
 var app = express();
 app.use(bodyParser.json());
 app.use(cors({ origin: 'http://localhost:4200' }));
 
 app.listen(3000, () => console.log('Server started at port : 3000'));
+
+
+app.use('/api/v1/demo', sampleController);
