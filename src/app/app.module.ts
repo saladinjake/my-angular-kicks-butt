b9c4caa525dc05b9fd7fe0519f031170048a74aa
saladinjake <juwavictor@gmail.com>
2020-09-05 21:35:17 +0100

    ch-#1 Created blank components for application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { WidgetsComponent } from './widgets/widgets.component';
+import { VideoComponent } from './widgets/video/video.component';
+import { ImagesComponent } from './widgets/images/images.component';
+import { BannersComponent } from './widgets/banners/banners.component';
+import { LandingComponent } from './landing/landing.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    WidgetsComponent,
+    VideoComponent,
+    ImagesComponent,
+    BannersComponent,
+    LandingComponent,
+    DashboardComponent,
+    LoginComponent,
+    RegisterComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
