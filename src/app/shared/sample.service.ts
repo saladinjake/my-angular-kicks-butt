ecfc231454e740ab23e5adae4546c19d3eaafa81
saladinjake <juwavictor@gmail.com>
2020-09-05 22:19:41 +0100

    ch-chore#7 Implemented dashboard component file for frontend

--- src/app/shared/sample.service.ts
+++ src/app/shared/sample.service.ts
@@ -8,24 +8,26 @@ import { Employee } from './sample.model';
 
 @Injectable()
 export class EmployeeService {
+  //one way to declear attributes of a class
   selectedEmployee: Employee;
   employees: Employee[];
   readonly baseURL = 'http://localhost:3000/employees';
 
-  constructor(private http: HttpClient) { }
-
-  postEmployee(emp: Employee) {
-    return this.http.post(this.baseURL, emp);
+  constructor(private http: HttpClient) { } // another way to declear attributes of a class by dependencies injection of  a contructor
+  //C: create
+  postEmployee(emp: Employee) { // dependency injection for a method
+    return this.http.post(this.baseURL, emp);  // this returns a promise
   }
-
+  //R: read
   getEmployeeList() {
     return this.http.get(this.baseURL);
   }
 
+  //U: update
   putEmployee(emp: Employee) {
     return this.http.put(this.baseURL + `/${emp._id}`, emp);
   }
-
+  //D: delete
   deleteEmployee(_id: string) {
     return this.http.delete(this.baseURL + `/${_id}`);
   }
