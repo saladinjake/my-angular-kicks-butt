3f4cb9c3130c1be7f29e1d54e9264e1554b19ed6
saladinjake <juwavictor@gmail.com>
2020-09-05 21:44:08 +0100

    ch-#3 Created model sample  file

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-
+const { mongoose } = require('./db.js');
 
 var app = express();
 app.use(bodyParser.json());
