a7a4e13da76e5173c0fc1043467682afaf686e53
saladinjake <juwavictor@gmail.com>
2020-09-14 21:42:33 +0100

    ch-#(ch-node js) Code refactor for routes into controllers

---
+++ server/controllers/secured/user.js
@@ -0,0 +1,8 @@
+export class  UserControllerResource{
+  static index(req,res){}
+  static show(req,res){}
+  static create(req,res){}
+  static edit(req,res){}
+  static update(req,res){}
+  static delete(req,res){}
+}
--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -4,31 +4,11 @@ const router = express.Router();
 const authenticate = require('../middlewares/authenticate');
 const User = require('../models/user');
 
+const UserController = require("../controllers/auth.controller")
 
-router.post('/register', (req, res) => {
-  const newUser = new User({
-    username: req.body.username,
-    email: req.body.email,
-  })
-  newUser.password = newUser.generateHash(req.body.password);
-  newUser.save().then(rec => {
-    res.status(201).json(rec)
-  })
-})
+router.post('/register', UserController.register)
 
-router.post('/login', (req, res) => {
-  User.findOne({email: req.body.email}).then(loginUser => {
-    if(!loginUser) {
-      return res.status(401).json({message: 'Invalid username or password'})
-    }
-    if(!loginUser.validatePassword(req.body.password)) {
-      return res.status(401).json({message: 'Invalid username or password'})
-    }
-    const withTokem = {email: loginUser.email, _id: loginUser._id};
-    withTokem.token = loginUser.generateJWT();
-    res.status(200).json(withTokem)
-  })
-})
+router.post('/login', UserController.login)
 
 router.get('/users', authenticate, (req, res) => {
   User.find().then(rec => {
