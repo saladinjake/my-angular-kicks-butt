3f9a45c1716e32553bc35fc9c102b28b26279a57
saladinjake <juwavictor@gmail.com>
2020-10-10 14:11:00 +0100

    Add change password functionality

--- src/app/core/components/changepassword/changepassword.component.ts
+++ src/app/core/components/changepassword/changepassword.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import {
+
+   FormBuilder, AbstractControl,
+   FormGroup, Validators
+ } from '@angular/forms'
 
 @Component({
   selector: 'app-changepassword',
   templateUrl: './changepassword.component.html',
-  styleUrls: ['./changepassword.component.css']
+  styleUrls: ['./changepassword.component.css'],
+  // directives: [REACTIVE_FORM_DIRECTIVES]
+
 })
 export class ChangepasswordComponent implements OnInit {
+  pwChangeForm: FormGroup;
+ // Properties that store paths to FormControls makes our template less verbose
+ current: AbstractControl;
+ newPW: AbstractControl;
+ confirm: AbstractControl;
 
-  constructor() { }
+ constructor(private fb: FormBuilder) { }
+ ngOnInit() {
+    this.pwChangeForm = this.fb.group({
+      current: ['', Validators.required],
+      newPW: ['', Validators.required],
+      confirm: ['', Validators.required]
+    }, {
+       // Here we create validators to be used for the group as a whole
+         validator: Validators.compose([
+            PWChangeValidators.newIsNotOld,
+            PWChangeValidators.newMatchesConfirm
+         ])
+  });
 
-  ngOnInit(): void {
+ this.current = this.pwChangeForm.controls['current'];
+ this.newPW = this.pwChangeForm.controls['newPW'];
+ this.confirm = this.pwChangeForm.controls['confirm'];
+
   }
 
 }
